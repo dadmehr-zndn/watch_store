e1f6b0b337d22e951cac830137bbda1df595ea8e
dadmehr-zndn <dadmehrzandian@gmail.com>
2024-07-28 14:15:19 +0330

    colors completely added

---
+++ lib/resources/colors.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+
+class LightAppColors {
+  LightAppColors._();
+
+  // S O L I D
+  static const Color primary = Color(0xff0075FB);
+  static const Color surface = Color(0xffFBFBFB);
+
+  static const Color textHint = Color(0xffD9DCE4);
+  static const Color textFieldLabel = Color(0xff000000);
+  static const Color textFieldBorder = Color(0xffD9DCE4);
+  static const Color textFieldFill = Color(0xffFFFFFF);
+  static const Color textFieldBorderSelected = Color(0xff0075FB);
+  static const Color textFieldPrefixIcon = Color(0xff000000);
+
+  static const Color button = Color(0xff0075FB);
+  static const Color buttonText = Color(0xffFFFFFF);
+
+  static const Color editNumber = Color(0xff0075FB);
+
+  static const Color appBarBG = Color(0xffFFFFFF);
+  static const Color appBarTitle = Color(0xff000000);
+  static const Color appBarIcon = Color(0xff000000);
+
+  static const Color searchBarIcon = Color(0xff000000);
+  static const Color searchBarTextHint = Color(0xffABABAB);
+
+  static const Color sliderBorder = Color(0xff707070);
+  static const Color sliderNotSelectedFill = Color(0xffFFFFFF);
+  static const Color sliderSelectedFill = Color(0xffC1C1C1);
+
+  static const Color seeAll = Color(0xff000000);
+  static const Color amazing = Color(0xff391850);
+  static const Color bestSelling = Color(0xffCF2755);
+  static const Color newest = Color(0xff777777);
+
+  static const Color productTitle = Color(0xff333333);
+  static const Color productCertainPrice = Color(0xff000000);
+  static const Color productPreviousPrice = Color(0xffBFBFBF);
+  static const Color productDiscountCountdownTimer = Color(0xff0075FB);
+  static const Color discountPercentBG = Color(0xffFF3A3A);
+  static const Color discountPercentText = Color(0xffFFFFFF);
+
+  static const Color navBarBG = Color(0xffFFFFFF);
+  static const Color navBarIcon = Color(0xffCECECE);
+  static const Color navBarIconText = Color(0xffC4C4C4);
+  static const Color navBarIconSelected = Color(0xff000000);
+  static const Color navBarIconSelectedText = Color(0xff000000);
+
+  static const Color cartCountItemBG = Color(0xffFF0000);
+  static const Color cartCountItemNum = Color(0xffFFFFFF);
+
+  static const Color closeIcon = Color(0xff707070);
+
+  static const Color brandCategoryBG = Color(0xff0075FB);
+  static const Color brandCategoryText = Color(0xffFFFFFF);
+
+  static const Color watchBrand = Color(0xff000000);
+  static const Color watchHeadline = Color(0xff545454);
+  static const Color watchFeature = Color(0xff545454);
+  static const Color tabBarBorder = Color(0xffF8F8F8);
+  static const Color tabBarItem = Color(0xffB5B5B5);
+  static const Color tabBarSelectedItem = Color(0xff000000);
+
+  static const Color sendToAddress = Color(0xff888D9B);
+  static const Color arrowLeft = Color(0xff292D32);
+
+  static const Color cartProductBG = Color(0xffF3F3F3);
+  static const Color cartProductPrice = Color(0xff888D9B);
+  static const Color cartProductDiscount = Color(0xff0075FB);
+  static const Color cartProductInsideContainerDivider = Color(0xffD9DCE4);
+  static const Color cartProductIcon = Color(0xff888D9B);
+  static const Color cartProductCount = Color(0xff000000);
+  static const Color cartProductButton = Color(0xffFF6161);
+
+  static const Color profileSectionText = Color(0xff000000);
+  static const Color profileSectionIcon = Color(0xff292D32);
+  static const Color statusText = Color(0xff505050);
+
+  // G R A D I N T S
+  static const List<Color> catClassic = [
+    Color(0xffFFA672),
+    Color(0xffFFE3C8),
+  ];
+  static const List<Color> catSmart = [
+    Color(0xff62909C),
+    Color(0xffDFEEF5),
+  ];
+  static const List<Color> catDigital = [
+    Color(0xffE183D4),
+    Color(0xffFFE6F5),
+  ];
+  static const List<Color> catDesktop = [
+    Color(0xff8396E1),
+    Color(0xffE6FCFF),
+  ];
+  static const List<Color> productBG = [
+    Color(0xffEEEEEE),
+    Color(0xffFFFFFF),
+  ];
+}
