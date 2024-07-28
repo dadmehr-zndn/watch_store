6ec6db68efa5fd570ec2ef7969bdbfae4b5dead6
dadmehr-zndn <dadmehrzandian@gmail.com>
2024-07-28 14:34:14 +0330

    strings completely added

---
+++ lib/resources/strings.dart
@@ -0,0 +1,63 @@
+class AppStrings {
+  AppStrings._();
+
+  static const String enterYourNumber = 'شماره خود را وارد کنید';
+  static const String enterYourNumberHint = 'مثل :  09121114466';
+  static const String sendActivationCode = 'ارسال کد فعال سازی';
+
+  static const String activationCodeSentToNumber =
+      'کد فعال سازی برای 09112223344 ارسال شد';
+  static const String wrongNumberOrEdit = 'شماره اشتباه است/ ویرایش شماره';
+  static const String enterActivationCode = 'کد فعال سازی را وارد کنید';
+  static const String timeRemainedCode = '2:05';
+  static const String enterActivationCodeHint = '- - - - - -';
+  static const String continueButton = 'ادامه';
+
+  static const String register = 'ثبت‌نام';
+  static const String chooseProfile = 'انتخاب تصویر پروفایل';
+  static const String fullname = 'نام و نام خانوادگی';
+  static const String fullnameHint =
+      'نام و نام خانوادگی خود را با حروف فارسی وارد کنید';
+  static const String landlinePhone = 'تلفن ثابت';
+  static const String landlinePhoneHint =
+      'شماره تلفن ثابت را با پیش شماره وارد بفرمایید';
+  static const String address = 'آدرس';
+  static const String addressHint = 'آدرس پستی خود را وارد کنید';
+  static const String postalCode = 'کد پستی';
+  static const String postalCodeHint = 'کد پستی  10 رقمی خود را وارد کنید';
+  static const String location = 'موقعیت مکانی';
+  static const String locationHint = 'برای انتخاب موقعیت مکانی ضربه برنید';
+
+  static const String search = 'جستجوی محصولات';
+  static const String classic = 'کلاسیک';
+  static const String smart = 'هوشمند';
+  static const String digital = 'دیجیتال';
+  static const String desktop = 'رومیزی';
+  static const String amazing = ' شگفت انگیز';
+  static const String bestSellings = 'پرفروش‌ها';
+  static const String newest = 'جدیدترین';
+  static const String seeAll = 'مشاهده همه';
+
+  static const String home = 'خانه';
+  static const String cart = 'سبد خرید';
+  static const String profile = 'پروفایل';
+
+  static const String features = 'خصوصیات';
+  static const String review = 'نقد و بررسی';
+  static const String comments = 'نظرات';
+  static const String addToCart = 'افزودن به سبد خرید';
+
+  static const String sendToAddress = 'ارسال به آدرس';
+  static const String price = 'قیمت';
+  static const String discount = 'با تخفیف';
+  static const String count = 'عدد';
+  static const String continueBuying = 'ادامه فرآیند خرید';
+  static const String totalPrice = 'مجموع';
+
+  static const String profleAppBar = 'پروفایل کاربر';
+  static const String username = 'دادمهر زندیان';
+  static const String termAndConditions = 'قوانین و مقررات';
+  static const String proccessing = 'در حال پردازش';
+  static const String cancelled = 'لغو شده';
+  static const String delivered = 'تحویل';
+}
