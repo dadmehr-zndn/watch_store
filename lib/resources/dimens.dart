33451fd0645fddb1b94258bd07faf290c1fa1818
dadmehr-zndn <dadmehrzandian@gmail.com>
2024-07-29 16:11:55 +0330

    some dimens added

--- lib/resources/dimens.dart
+++ lib/resources/dimens.dart
@@ -1,7 +1,18 @@
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
 class Dimens {
   Dimens._();
 
   static const double small = 8;
   static const double medium = 12;
   static const double large = 24;
+
+  static double textFieldDistance = 17.h;
+  static double textFieldContentPadding = 13.w;
+  static double textFieldIconPadding = 11.w;
+
+  static double registerAppBarHeight = 67.h;
+  static double registerAppBarHorizontalPadding = 19.25.w;
+
+  static double btmNavBar = 83.h;
 }
