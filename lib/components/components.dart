1e890a258a1b506ea5bcab5bff640003d589ae81
dadmehr-zndn <dadmehrzandian@gmail.com>
2024-07-29 16:10:40 +0330

    barrel files added

---
+++ lib/components/components.dart
@@ -0,0 +1,5 @@
+export 'button_styles.dart';
+export 'text_styles.dart';
+export 'decorations.dart';
+export 'extensions.dart';
+export 'themes.dart';
---
+++ lib/screens/screens.dart
@@ -0,0 +1,7 @@
+export 'send_code_screen.dart';
+export 'check_code_screen.dart';
+export 'register_screen.dart';
+export 'main_screen/main_screen.dart';
+export 'main_screen/home_screen.dart';
+export 'main_screen/cart_screen.dart';
+export 'main_screen/profile_screen.dart';
