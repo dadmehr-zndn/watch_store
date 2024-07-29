45618787cd5607d79f9b7367765462e00e335030
dadmehr-zndn <dadmehrzandian@gmail.com>
2024-07-29 16:12:54 +0330

    Screen Design Completed

---
+++ lib/screens/register_screen.dart
@@ -0,0 +1,99 @@
+// TODO: uncomment
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
+
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:watch_store/components/extensions.dart';
+import 'package:watch_store/components/text_styles.dart';
+import 'package:watch_store/gen/assets.gen.dart';
+import 'package:watch_store/resources/resources.dart';
+import 'package:watch_store/routes/routes_names.dart';
+import 'package:watch_store/widgets/app_bars/register_app_bar.dart';
+import 'package:watch_store/widgets/avatar.dart';
+import 'package:watch_store/widgets/icons/location_icon.dart';
+import 'package:watch_store/widgets/primary_button.dart';
+import 'package:watch_store/widgets/text_field.dart';
+
+class RegisterScreen extends StatelessWidget {
+  RegisterScreen({super.key});
+  TextEditingController _textEditingController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        appBar: WatchStoreRegisterAppBar(),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Avatar(
+                text: AppStrings.chooseProfile,
+              ),
+              38.h.height,
+
+              // F U L L   N A M E
+              WatchStoreTextField(
+                controller: _textEditingController,
+                label: AppStrings.fullname,
+                hint: AppStrings.fullnameHint,
+                textAlign: TextAlign.end,
+              ),
+
+              Dimens.textFieldDistance.height,
+
+              // L A N D I N G   N U M B E R
+              WatchStoreTextField(
+                controller: _textEditingController,
+                label: AppStrings.landlinePhone,
+                hint: AppStrings.landlinePhoneHint,
+                textAlign: TextAlign.end,
+              ),
+
+              Dimens.textFieldDistance.height,
+
+              // A D D R E S S
+              WatchStoreTextField(
+                controller: _textEditingController,
+                label: AppStrings.address,
+                hint: AppStrings.addressHint,
+                textAlign: TextAlign.end,
+              ),
+
+              Dimens.textFieldDistance.height,
+
+              // P O S T A L   C O D E
+              WatchStoreTextField(
+                controller: _textEditingController,
+                label: AppStrings.postalCode,
+                hint: AppStrings.postalCodeHint,
+                textAlign: TextAlign.end,
+              ),
+
+              Dimens.textFieldDistance.height,
+
+              // L O C A T I O N
+              WatchStoreTextField(
+                controller: _textEditingController,
+                label: AppStrings.location,
+                hint: AppStrings.locationHint,
+                textAlign: TextAlign.end,
+                assetIcon: WatchStoreLocationIcon(),
+              ),
+
+              29.h.height,
+
+              // R E G I S T E R   B U T T O N
+              WatchStorePrimaryButton(
+                text: AppStrings.register,
+                onPressed: () =>
+                    Navigator.pushNamed(context, RoutesNames.mainScreen),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/screens/send_code_screen.dart
@@ -0,0 +1,45 @@
+// TODO: uncomment
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
+
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:watch_store/resources/strings.dart';
+import 'package:watch_store/routes/routes_names.dart';
+import 'package:watch_store/widgets/logo.dart';
+import 'package:watch_store/widgets/primary_button.dart';
+import 'package:watch_store/widgets/text_field.dart';
+import '../components/extensions.dart';
+
+class SendCodeScreen extends StatelessWidget {
+  const SendCodeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            // crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              WatchStoreLogo(),
+              116.h.height,
+              WatchStoreTextField(
+                controller: TextEditingController(),
+                label: AppStrings.enterYourNumber,
+                hint: AppStrings.enterYourNumberHint,
+                textAlign: TextAlign.center,
+              ),
+              24.h.height,
+              WatchStorePrimaryButton(
+                onPressed: () =>
+                    Navigator.pushNamed(context, RoutesNames.checkCodeScreen),
+                text: AppStrings.sendActivationCode,
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
