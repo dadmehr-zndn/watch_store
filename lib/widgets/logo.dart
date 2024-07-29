14402970093e8d368829a6baf7ac63b8e34547c1
dadmehr-zndn <dadmehrzandian@gmail.com>
2024-07-29 07:08:03 +0330

    WatchStoreLogo widget

---
+++ lib/widgets/logo.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:watch_store/gen/assets.gen.dart';
+
+class WatchStoreLogo extends StatelessWidget {
+  const WatchStoreLogo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Image.asset(
+      Assets.png.lgoo.path,
+      height: 121.h,
+      width: 300.w,
+    );
+  }
+}
