a84c6eef3a78d1959bffcc56db4c2c421f6d7cfa
dadmehr-zndn <dadmehrzandian@gmail.com>
2024-07-31 16:38:26 +0330

    Logo Widget now is customizable

--- lib/widgets/logo.dart
+++ lib/widgets/logo.dart
@@ -3,14 +3,20 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:watch_store/gen/assets.gen.dart';
 
 class WatchStoreLogo extends StatelessWidget {
-  const WatchStoreLogo({super.key});
+  double? height = 121.h;
+  double? width = 300.h;
+  WatchStoreLogo({
+    super.key,
+    this.height,
+    this.width,
+  });
 
   @override
   Widget build(BuildContext context) {
     return Image.asset(
       Assets.png.logo.path,
-      height: 121.h,
-      width: 300.w,
+      height: height,
+      width: width,
     );
   }
 }
