660a2a59e67f7828c55ea8cb2f2946afd14b081a
dadmehr-zndn <dadmehrzandian@gmail.com>
2024-10-03 07:53:46 +0330

    some addresses added

--- lib/resources/strings.dart
+++ lib/resources/strings.dart
@@ -24,6 +24,7 @@ class AppStrings {
   static const String landlinePhoneHint =
       'شماره تلفن ثابت را با پیش شماره وارد بفرمایید';
   static const String address = 'آدرس';
+  static const String activeAddress = 'فعال آدرس';
   static const String addressHint = 'آدرس پستی خود را وارد کنید';
   static const String postalCode = 'کد پستی';
   static const String postalCodeHint = 'کد پستی  10 رقمی خود را وارد کنید';
@@ -37,6 +38,7 @@ class AppStrings {
   static const String desktop = 'رومیزی';
   static const String amazing = ' شگفت انگیز';
   static const String bestSellings = 'پرفروش‌ها';
+  static const String mostSellings = 'پرفروش‌‌ترین ها';
   static const String newest = 'جدیدترین';
   static const String seeAll = 'مشاهده همه';
 
@@ -61,9 +63,29 @@ class AppStrings {
   static const String termAndConditions = 'قوانین و مقررات';
   static const String proccessing = 'در حال پردازش';
   static const String cancelled = 'لغو شده';
-  static const String delivered = 'تحویل';
+  static const String delivered = 'تحویل شده';
 
   // TODO: delete these fake strings
   static const String fakeProductName = 'ساعت مردانه نیوفورس';
   static const String fakeProductDiscountCountdown = '09:26:22';
+  static const String fakeBrandCasio = 'کاسیو';
+  static const String fakeCartCountItem = '2';
+  static const String fakeSingleProductFullName = 'ساعت شیائومی mi Watch lite';
+  static const String fakeProductCardTitle = 'بنسر';
+  static const String fakeProductCardDetail =
+      'مسواک بنسر مدل اکسترا با برس متوسط 3 عددی';
+  static const String fakeProductFeatures = 'خصوصیات';
+  static const String fakeProductReview = 'نقد و بررسی';
+  static const String fakeProductComments = 'نظرات';
+  static const String fakeLoremSendToAddress =
+      'لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ ';
+  static const String fakeProductLorem =
+      '''لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد. کتابهای زیادی در شصت و سه درصد گذشته، حال و آینده شناخت فراوان جامعه و متخصصان را می طلبد تا با نرم افزارها شناخت بیشتری را برای طراحان رایانه ای علی الخصوص طراحان خلاقی و فرهنگ پیشرو در زبان فارسی ایجاد کرد. در این صورت می توان امید داشت که تمام و دشواری موجود در ارائه راهکارها و شرایط سخت تایپ به پایان رسد وزمان مورد نیاز شامل حروفچینی دستاوردهای اصلی و جوابگوی سوالات پیوسته اهل دنیای موجود طراحی اساسا مورد استفاده قرار گیرد.
+''';
+
+  static const List<String> fakeTabViewItems = [
+    fakeProductFeatures,
+    fakeProductReview,
+    fakeProductComments,
+  ];
 }
