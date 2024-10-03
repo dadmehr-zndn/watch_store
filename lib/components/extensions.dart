f0cf05ef51d7df43c781ba8a5aa34d8424090aa7
dadmehr-zndn <dadmehrzandian@gmail.com>
2024-10-03 07:52:30 +0330

    extension added for price formatting

--- lib/components/extensions.dart
+++ lib/components/extensions.dart
@@ -1,6 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 extension CustomSizedBox on double {
   SizedBox get height => SizedBox(height: this);
   SizedBox get width => SizedBox(width: this);
+}
+
+extension PriceDivider on int {
+  String toPrice() {
+    final pattern = NumberFormat.decimalPattern();
+    final formatedPrice = pattern.format(this);
+    return formatedPrice;
+  }
 }
