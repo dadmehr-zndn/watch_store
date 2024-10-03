8d8a7126e94d63f23bdf52d3fb2ec30e2793eebd
dadmehr-zndn <dadmehrzandian@gmail.com>
2024-10-03 07:56:26 +0330

    Refactored

--- lib/screens/screens.dart
+++ lib/screens/screens.dart
@@ -1,6 +1,6 @@
-export 'send_code_screen.dart';
-export 'check_code_screen.dart';
-export 'register_screen.dart';
+export 'auth/send_code_screen.dart';
+export 'auth/verify_code_screen.dart';
+export 'auth/register_screen.dart';
 export 'main_screen/main_screen.dart';
 export 'main_screen/home_screen.dart';
 export 'main_screen/cart_screen.dart';
